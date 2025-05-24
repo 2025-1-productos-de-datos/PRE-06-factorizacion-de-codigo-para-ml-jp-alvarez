14792d59445645cf98f7dedb3b64a5fd7aba33f2
jp-alvarez <jualvarez@unal.edu.co>
2025-05-24 08:03:11 -0500

    intento1

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+"""Entry point for the homework package."""
+
+# python3 -m homework --model elasticnet --l1 ratio 0.1 --alpha 0.1
+
+from .src.main import main
+
+if __name__ == "__main__":
+    main()
